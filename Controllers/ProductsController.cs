23ea42ff0abbb02af48eb1243d342c4ea0c82bab
MainaGeorge <nuriszka@gmail.com>
2020-05-13 10:31:54 +0200

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HPlusSport.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HPlusSport.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopContext _context;
+
+        public ProductsController(ShopContext context)
+        {
+            _context = context;
+
+            _context.Database.EnsureCreated();
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _context.Products.ToArrayAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json;
+
+namespace HPlusSport.API.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public int UserId { get; set; }
+
+        [JsonIgnore]
+        public virtual User User { get; set; }
+
+        public List<Product> Products { get; set; }
+
+
+    }
+}
