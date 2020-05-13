19956a0f11c28a7bd2470ceb75544c31fe71dc56
MainaGeorge <nuriszka@gmail.com>
2020-05-13 10:40:12 +0200

    Add helper class page and size control

---
+++ HelperClasses/PageSizeModifiers.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace HPlusSport.API.HelperClasses
+{
+    public class PageSizeModifiers
+    {
+
+        private int _maxSizePerPage = 15;
+
+        public int PageNumber { get; set; }
+
+        public int Size
+        {
+            get => _maxSizePerPage;
+
+            set
+            {
+                if (value > 0 && value < _maxSizePerPage)
+                {
+                    _maxSizePerPage = value;
+                }
+            }
+        }
+
+    }
+}
